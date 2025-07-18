4e6dc2927d0afecf9787bdeebc1bf4c46877fc29
Holden Phillips <s5340515@student.ad.griffith.edu.au>
2025-07-18 15:50:41 +1000

    Junkie Enemy Movement
    The Junkie Enemy can now move around the map however it is very slow going.

--- Assets/Scripts/Enemy_AI_Movement.cs
+++ Assets/Scripts/Enemy_AI_Movement.cs
@@ -7,23 +7,25 @@ public class Enemy : MonoBehaviour
 {
     public NavMeshAgent agent;
     public Vector3 RandomPoint;
-    public Vector3 RandomPointOnTerrain;
+    public Vector3 RandomPointOnPlane;
     public bool isPatrolling;
+    public float speed;
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-
+        speed = 10;
     }
     void Update()
     {
         if (isPatrolling == false)
-        { //it finished the current patrolle and need other point
+        { //it finished the current patrol and need other point
             RandomPoint = Random.insideUnitSphere * 6; //find point in sphere
-            RandomPointOnTerrain = new Vector3(RandomPoint.x, Terrain.activeTerrain.SampleHeight(RandomPoint), RandomPoint.z); //find the random point on terrain
-            agent.SetDestination(RandomPointOnTerrain); //set the patrolling target
-            isPatrolling = true //it received a target
+            RandomPointOnPlane = new Vector3(RandomPoint.x, RandomPoint.y, RandomPoint.z); //find the random point on terrain
+            agent.SetDestination(RandomPointOnPlane); //set the patrolling target
+            isPatrolling = true; //it received a target
     }
-        if (agent.remainingDistance - agent.stoppingDistance)
+        if (agent.remainingDistance > 0)
         {
             isPatrolling = true; //still patrolling
         }
@@ -31,5 +33,5 @@ public class Enemy : MonoBehaviour
         {
             isPatrolling = false; //it finished the current patrolle and need other point
         }
-
+    }
 }
